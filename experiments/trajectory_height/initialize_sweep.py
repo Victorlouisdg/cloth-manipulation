3d2b4154d6737f3a5031e6ba92341314ba90fed4
victorlouisdg <victorlouisdg@gmail.com>
2021-12-16 08:42:26 +0100

    wandb sweeps

---
+++ experiments/trajectory_height/initialize_sweep.py
@@ -0,0 +1,15 @@
+import wandb
+import pprint
+
+sweep_config = {
+    "project": "cloth-manipulation",
+    "entity": "victorlouis",
+    "name": "my-sweep",
+    "method": "random",
+    "metric": {"goal": "minimize", "name": "mean_distance"},
+    "parameters": {"height": {"min": 0.2, "max": 0.6}},
+}
+
+sweep_id = wandb.sweep(sweep_config)
+print(sweep_id)
+pprint.pprint(sweep_config)
