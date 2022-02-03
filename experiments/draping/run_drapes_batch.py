8ff9c0acd97abe367cbc2ff7be2f9960df53b60e
victorlouisdg <victorlouisdg@gmail.com>
2022-02-03 10:07:45 +0100

    drape batch

---
+++ experiments/draping/run_drapes_batch.py
@@ -0,0 +1,13 @@
+import os
+import sys
+
+sys.path.append(os.path.dirname(__file__))
+
+from simulate_drape import run_drape_simulation
+
+from cm_utils.materials.penava import materials
+
+for material in materials:
+    run_drape_simulation(
+        sphere_radius=0.1, cloth_size=0.45, cloth_subdivisions=8, cloth_material=material, simulation_steps=25
+    )
