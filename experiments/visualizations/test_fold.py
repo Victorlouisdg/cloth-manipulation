d5ef92fbf20850a2675e4b16b48ececd4da9bec2
victorlouisdg <victorlouisdg@gmail.com>
2022-02-24 11:21:57 +0100

    simplify paths

--- experiments/visualizations/test_fold.py
+++ experiments/visualizations/test_fold.py
@@ -2,7 +2,7 @@ import airo_blender_toolkit as abt
 import bpy
 import numpy as np
 
-from cm_utils.folds import SleeveFold, TiltedEllipticalArcFoldTrajectory
+from cm_utils.folds import EllipticalFoldTrajectory, SleeveFold
 from cm_utils.keyframe import keyframe_trajectory
 
 keypoint_ids_cloth = {
@@ -30,7 +30,7 @@ keypoints = {name: np.array(cloth.data.vertices[id].co) for name, id in keypoint
 fold = SleeveFold(keypoints, "left")
 
 
-fold_trajectory = TiltedEllipticalArcFoldTrajectory(fold, tilt_angle=60)
+fold_trajectory = EllipticalFoldTrajectory(fold, scale=2.0, tilt_angle=60, orientation_mode="constant")
 
 abt.visualize_transform(fold_trajectory.path._rotation_basis(), 0.2)
 
@@ -39,7 +39,7 @@ for time_completion in np.linspace(0, 1, 20):
     pose = fold_trajectory.pose(time_completion)
     abt.visualize_transform(pose, 0.05)
 
-empty = abt.visualize_transform(fold_trajectory.start(), 0.1)
+empty = abt.visualize_transform(fold_trajectory.start, 0.1)
 keyframe_trajectory(empty, fold_trajectory, 0, 100)
 
 fold_trajectory.path.check_arc_length_parametrization()
