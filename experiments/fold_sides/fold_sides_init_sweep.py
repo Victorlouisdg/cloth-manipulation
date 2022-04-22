72295fff1b8f473aa7959240492571f8d8b2eba3
victorlouisdg <victorlouisdg@gmail.com>
2022-04-22 12:22:38 +0200

    fold sides sweep

---
+++ experiments/fold_sides/fold_sides_init_sweep.py
@@ -0,0 +1,29 @@
+import pprint
+
+import numpy as np
+import wandb
+
+values = []
+
+for height_ratio in np.linspace(0.1, 1.0, 14):
+    for angle in np.linspace(30.0, 90.0, max(2, int(18 * height_ratio))):
+        tilt_angle = 90.0 - angle
+        values.append(f"{height_ratio}-{tilt_angle}")
+
+
+sweep_config = {
+    "entity": "victorlouis",
+    "name": "sweep0",
+    "method": "grid",
+    "metric": {"goal": "minimize", "name": "mean_distance"},
+    "parameters": {
+        "height_ratio-tilt_angle": {"values": values},
+    },
+    "project": "fold_sides",
+}
+
+sweep_id = wandb.sweep(sweep_config, project=sweep_config["project"])
+print(sweep_id)
+print("Combinations:", len(values))
+pprint.pprint(sweep_config)
+print(sweep_id)
---
+++ experiments/run_fold_wandb.py
@@ -0,0 +1,67 @@
+import argparse
+import json
+import os
+import shutil
+import subprocess
+import sys
+from functools import partial
+
+import wandb
+
+
+def parse_parameters(run):
+    value = run.config["height_ratio-tilt_angle"]
+    height_ratio, tilt_angle = value.split("-")
+    height_ratio = float(height_ratio)
+    tilt_angle = float(tilt_angle)
+    return height_ratio, tilt_angle
+
+
+def make_output_dir(run_name, height_ratio, tilt_angle):
+    dir = os.path.dirname(os.path.abspath(__file__))
+    wandb_dir = os.path.join(dir, "output", run_name)
+    subdir = os.path.join(wandb_dir, f"height_ratio {height_ratio:.4f} tilt_angle {tilt_angle}")
+    os.makedirs(subdir)
+    return subdir
+
+
+def log_results(height_ratio, tilt_angle, output_dir):
+    log = {"height_ratio": height_ratio, "tilt_angle": tilt_angle}
+    with open(os.path.join(output_dir, "losses.json")) as f:
+        losses = json.load(f)
+    log = log | losses
+    wandb.log(log)
+    wandb.log({"result": wandb.Image(os.path.join(output_dir, "result.png"))})
+
+
+def run_wandb(script, keep_output=False):
+    with wandb.init() as run:
+        height_ratio, tilt_angle = parse_parameters(run)
+        output_dir = make_output_dir(run.name, height_ratio, tilt_angle)
+
+        runCommand = f"blender -b -P {script} -- -ht {height_ratio} -ta {tilt_angle} -d '{output_dir}'"
+        subprocess.run([runCommand], shell=True, stdout=subprocess.DEVNULL)
+
+        log_results(height_ratio, tilt_angle, output_dir)
+
+        if not keep_output:
+            shutil.rmtree(output_dir)
+
+
+if __name__ == "__main__":
+    if "--" in sys.argv:
+        arg_start = sys.argv.index("--") + 1
+        argv = sys.argv[arg_start:]
+        parser = argparse.ArgumentParser()
+        parser.add_argument("script", help="The python script of the experiment.")
+        parser.add_argument("project", help="The project on wandb to log to.")
+        parser.add_argument("sweep_id")
+        parser.add_argument("-c", "--count", dest="count", type=int, help="Amount of runs for the wandb agent.")
+        parser.add_argument(
+            "--keep_output",
+            action=argparse.BooleanOptionalAction,
+            help="If not set all simulation output will be removed after the run to save memory.",
+        )
+        args = parser.parse_known_args(argv)[0]
+        wandb_function = partial(run_wandb, script=args.script, keep_output=args.keep_output)
+        wandb.agent(args.sweep_id, project=args.project, function=wandb_function, count=args.count)
