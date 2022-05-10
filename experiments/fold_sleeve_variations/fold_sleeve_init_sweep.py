f6873ae940436eb3932ad390c18aac2e8b8195ae
victorlouisdg <victorlouisdg@gmail.com>
2022-05-10 11:04:34 +0200

    fold sleeve sweep thick

--- experiments/fold_sleeve_variations/fold_sleeve_init_sweep.py
+++ experiments/fold_sleeve_variations/fold_sleeve_init_sweep.py
@@ -19,7 +19,7 @@ sweep_config = {
     "parameters": {
         "height_ratio-tilt_angle": {"values": values},
     },
-    "project": "fold_sleeve_thin",
+    "project": "fold_sleeve_thick",
 }
 
 sweep_id = wandb.sweep(sweep_config, project=sweep_config["project"])
